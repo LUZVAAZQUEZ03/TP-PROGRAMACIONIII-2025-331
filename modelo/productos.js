261cd3784d634c7cb1f2fbe2df8345b42821b6cd
MicaUtn <micatorales12gmail.com>
2025-05-25 17:45:08 -0300

    prodcutos

--- modelo/productos.js
+++ modelo/productos.js
@@ -1,9 +1,10 @@
-class producto{
-    constructor(id, nombre, precio, cantidad, fotoProducto){
+class Producto {
+    constructor(id, nombre, precio, cantidad, fotoProducto, categoria) {
         this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.stock = cantidad;
         this.fotoProducto = fotoProducto;
+        this.categoria = categoria; // 'Perfume' o 'Skincare'
     }
 }
