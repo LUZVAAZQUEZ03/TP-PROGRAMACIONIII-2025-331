2d411edfc8f8285c67ce4d93ebc72acbd218a14e
MicaUtn <micatorales12gmail.com>
2025-05-31 00:19:06 -0300

    ordenando

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
 const express = require('express')
-const app = express()
-const cors = require('cors')
-const path = require('path')
+const cors = require('cors');
+const path = require('path');
+require('dotenv').config(); //npm install dotenv para utilizar el env y no tener puerto publico
+
 const rutasAdmin = require("./rutas/rutasAdmin.js")
 const rutasCliente = require("./rutas/rutasCliente.js");//exporto las rutas de los htmsls de cliente
-const port = 3000
 
+const port = process.env.PORT || 6000;
 
-//uso cors para activar todos los origenes
-app.use(cors());//si quisiera que sea solo para nuestro front lo pongo como: 
-// app.use(cors(origin: 'http://localhost:5502'));
+const configApi = (app) =>{
+        //uso cors para activar todos los origenes
+    app.use(cors());//si quisiera que sea solo para nuestro front lo pongo como: 
+    // app.use(cors(origin: 'http://localhost:5502'));
+    app.use(express.urlencoded({extended: true})); //permite que express entienda forms enviados por post
+    //sirvo estatico como estatico para poder iniciaer con el index
+    app.use(express.static(path.join(__dirname, 'estatico')));
+    app.set('view engine', 'ejs'); //seteo que las vistas se trrabajan con ejs
+    app.set('views', path.join(__dirname, 'vista')); //le paso el path de las vistas
 
-//sirvo estatico como estatico para poder iniciaer con el index
-app.use(express.static(path.join(__dirname, 'estatico')));
+    app.use('/', rutasAdmin); //cargo todas las vistas del admin
+    app.use('/cliente', rutasCliente); //activo las turas de los clientes con /cliente
+    return;
+}
 
-app.set('view engine', 'ejs'); //seteo que las vistas se trrabajan con ejs
-app.set('views', path.join(__dirname, 'vista')); //le paso el path de las vistas
-
-app.use('/', rutasAdmin); //cargo todas las vistas del admin
-app.use('/cliente', rutasCliente); //activo las turas de los clientes con /cliente
-
-
-//el index del programa
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + 'estatico'))
+const init = () =>{
+    const app = express();
+    configApi(app);
+    //el index del programa
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname + 'estatico'))
+    })
+    app.listen(port, '0.0.0.0',() => {
+        console.log(`Listening on port ${port}`)
 })
+}
 
-
-app.listen(port, '0.0.0.0',() => {
-    console.log(`Listening on port ${port}`)
-})
-
+init();
